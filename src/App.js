da48eaadbd3eb8dae91b2ccf76edb0798c13693d
Biraj Poddar <biraj.poddar@gmail.com>
2024-06-30 01:15:01 +0530

    1st Day code
    - component
    - Fragment uses

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ol>
+			<List />
+		</ol>
+	);
+}
+
+function List() {
+	const names = [
+		{ first: 'mannu', last: 'singh' },
+		{ first: 'biraj', last: 'poddar' },
+	];
+	return names.map((n) => {
+		return (
+			<Fragment>
+				<li>{n.first}</li>
+				<li>{n.last}</li>
+			</Fragment>
+		);
+	});
 }
 
 export default App;
